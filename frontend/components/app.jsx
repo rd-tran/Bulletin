cf3c46ab65954986099991542a152be9baf945ba
Ryan Tran <ryandtran@hotmail.com>
2020-06-30 01:05:43 -0700

    Pushing to heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils'
 import NavBarContainer from './navbar/navbar_container';
 import HomeContainer from './home/home_container';
