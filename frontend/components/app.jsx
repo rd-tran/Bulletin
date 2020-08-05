2a50024065534d800e5107422d3e53af53a65e19
Ryan Tran <ryandtran@hotmail.com>
2020-08-05 12:39:21 -0700

    Change Profile component to Protected Route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,7 +5,6 @@ import NavBarContainer from './navbar/navbar_container';
 import HomeContainer from './home/home_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import Profile from './profile/profile';
 
 const App = () => {
   return (
@@ -18,9 +17,7 @@ const App = () => {
           path="/signup"
           component={SignupFormContainer}
         />
-        <Route path="/u/:username">
-          <ProfileContainer/>
-        </Route>
+        <ProtectedRoute path="/u/:username" component={ProfileContainer}/>
         <Redirect to ="/" />
       </Switch>
     </div>
