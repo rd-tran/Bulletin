5242a679afc57288b83071b02c9ee4cd1231df8e
Ryan Tran <ryandtran@hotmail.com>
2020-08-06 08:22:11 -0700

    Cleanup

--- frontend/components/profile/intro/intro_container.jsx
+++ frontend/components/profile/intro/intro_container.jsx
@@ -3,8 +3,6 @@ import BioContainer from './bio/bio-container';
 import DetailsContainer from './details/details-container';
 
 function IntroContainer({ user }) {  
-  console.log(user);
-
   return (
     <div className="intro-container">
       <div className="header-container">
