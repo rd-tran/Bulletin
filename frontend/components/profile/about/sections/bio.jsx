443b4a8e8ebaa9540c5097f05cae95b782e2394b
Ryan Tran <ryandtran@hotmail.com>
2020-08-09 19:30:02 -0700

    Minor update

--- frontend/components/profile/about/sections/bio.jsx
+++ frontend/components/profile/about/sections/bio.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Bio = () => {
-
+  return (
+    <div className="bio">
+      Bio goes here
+    </div>
+  );
 };
 
 export default Bio;
--- frontend/components/profile/about/sections/hometown.jsx
+++ frontend/components/profile/about/sections/hometown.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Hometown = () => {
-
+  return (
+    <div className="hometown">
+      Hometown goes here
+    </div>
+  );
 };
 
 export default Hometown;
