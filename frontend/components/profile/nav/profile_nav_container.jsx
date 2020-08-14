bbf45c59d4bf18536cccfedd407ce10f43928209
Ryan Tran <ryandtran@hotmail.com>
2020-08-13 18:17:43 -0700

    Add active icon to profile nav buttons

--- frontend/components/profile/nav/profile_nav_container.jsx
+++ frontend/components/profile/nav/profile_nav_container.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const ProfileNavContainer = ({ match }) => {
+const ProfileNavContainer = ({ match, location }) => {
+  const pathParts = location.pathname.split('/');
+  const lastPathPart = pathParts[pathParts.length - 1];
+  console.log(lastPathPart)
+  const timelineActive = lastPathPart === 'timeline' ? 'active' : '';
+  const aboutActive = lastPathPart === 'about' ? 'active' : '';
+  const friendsActive = lastPathPart === 'friends' ? 'active' : '';
+  const photosActive = lastPathPart === 'photos' ? 'active' : '';
   
   return (
     <div className="profile-nav-container">
       <ul className='profile-nav'>
         <li className="profile-nav-button">
-          <Link to={`/u/${match.params.username}/timeline`}>Timeline</Link>
+          <Link to={`/u/${match.params.username}/timeline`}
+            className={timelineActive}
+          >
+            Timeline
+            <div className={`active-icon ${timelineActive}`}></div>
+          </Link>
         </li>
         <li className="profile-nav-button">
-          <Link to={`/u/${match.params.username}/about`}>About</Link>
+          <Link to={`/u/${match.params.username}/about`}
+            className={aboutActive}
+          >
+            About
+            <div className={`active-icon ${aboutActive}`}></div>
+          </Link>
         </li>
         <li className="profile-nav-button">
-          <Link to={`/u/${match.params.username}/friends`}>Friends</Link>
+          <Link to={`/u/${match.params.username}/friends`}
+            className={friendsActive}
+          >
+            Friends
+            <div className={`active-icon ${friendsActive}`}></div>
+          </Link>
         </li>
         <li className="profile-nav-button">
-          <Link to={`/u/${match.params.username}/photos`}>Photos</Link>
+          <Link to={`/u/${match.params.username}/photos`}
+            className={photosActive}
+          >
+            Photos
+            <div className={`active-icon ${photosActive}`}></div>
+          </Link>
         </li>
       </ul>
     </div>
